af83689184f422d48d2918637596fd88b37aef20
Amparo Jiménez <amparo_jd@hotmail.com>
2023-09-15 21:38:37 +0200

    15092023

---
+++ ft_putnbr_fd.c
@@ -0,0 +1,34 @@
+#include "libft.h"
+
+void	ft_putchar_fd(char c, int fd)
+{
+	write(fd, &c, 1);
+}
+
+void	ft_putnbr_fd(int n, int fd)
+{
+	long	num;
+
+	num = n;
+	if (num < 0)
+	{
+		ft_putchar_fd('-', fd);
+		num = num * -1;
+	}
+	if (num >= 10)
+	{
+		ft_putnbr_fd(num / 10, fd);
+		ft_putnbr_fd(num % 10, fd);
+	}
+	else
+	{
+		ft_putchar_fd(num + '0', fd);
+	}
+}
+
+/*int main(void)
+{
+    int n = 23;
+
+    ft_putnbr_fd(n, 1);
+}*/
