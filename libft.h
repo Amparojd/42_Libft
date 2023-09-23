56b8b56e40816e7dbe9f946758f145d6f6e8f6f2
ampjimen <ampjimen@student.42madrid.com>
2023-09-23 13:16:59 +0200

    23092023

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: ampjimen <ampjimen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/16 17:02:59 by ampjimen          #+#    #+#             */
-/*   Updated: 2023/09/16 17:03:54 by ampjimen         ###   ########.fr       */
+/*   Updated: 2023/09/23 12:40:41 by ampjimen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,9 +44,9 @@ char		*ft_strdup(const char *s1);
 char		*ft_substr(char const *s, unsigned int start, size_t len);
 char		*ft_strjoin(char const *s1, char const *s2);
 char		*ft_strtrim(char const *s1, char const *set);
-char		**ft_split(char const *s, char c);
-char		*ft_itoa(int n);
-char		*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+// char		**ft_split(char const *s, char c);
+// char		*ft_itoa(int n);
+//char		*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 void		ft_striteri(char *s, void (*f)(unsigned int, char *));
 void		ft_putchar_fd(char c, int fd);
 void		ft_putstr_fd(char *s, int fd);
